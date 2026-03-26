4186308724906dc4a1e86bdd473323407aa589e9
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-03-26 17:25:37 +0545

    Add image preview feature in create page

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -37,7 +37,7 @@
                 <div class="col-span-2">
                     <label class="block text-lg mb-2">Image*</label>
 
-                    <label for="image"
+                    <label for="image-js"
                         class="bg-white hover:bg-slate-100 border-2 border-zinc-400 rounded-md py-8 border-dotted flex flex-col justify-center items-center gap-2 cursor-pointer">
                         <span class="text-4xl text-zinc-500">
                             <i class="fa-solid fa-file-arrow-up"></i>
@@ -47,7 +47,22 @@
                         </span>
                     </label>
 
-                    <input type="file" name="image" id="image" class="hidden">
+                    <input type="file" name="image" id="image-js" class="hidden">
+                </div>
+
+                <div id="preview-container-js" class="hidden">
+                    <label class="block text-lg mb-2">Selected image</label>
+                    <div
+                        class="h-[142px] border-2 border-zinc-400 border-dotted rounded-md py-2 flex items-center justify-center">
+                        <div class="h-full relative">
+                            <img class="h-full" id="preview-js" src="#" alt="Preview">
+                            <button type="button"
+                                class="absolute -top-1.5 -right-1.5 h-6 w-6 flex items-center justify-center cursor-pointer text-sm text-white bg-red-600 rounded-full"
+                                id="remove-image-js">
+                                <i class="fa-solid fa-xmark"></i>
+                            </button>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="col-span-2">
@@ -90,4 +105,33 @@
         </form>
 
     </div>
+
+    <script>
+        document.getElementById("image-js").addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            const preview = document.getElementById("preview-js");
+            const previewContainer = document.getElementById("preview-container-js");
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                    previewContainer.classList.remove("hidden");
+                };
+
+                reader.readAsDataURL(file);
+            }
+        });
+
+        document.getElementById('remove-image-js').addEventListener('click', function() {
+            const image = document.getElementById('image-js');
+            const preview = document.getElementById("preview-js");
+            const previewContainer = document.getElementById('preview-container-js');
+
+            image.value = '';
+            previewContainer.classList.add('hidden');
+            preview.src = '#';
+        });
+    </script>
 </x-admin-layout>
