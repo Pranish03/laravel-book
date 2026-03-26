49f866dc2371e562f150bed7949ab2d6a5ec83c5
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-03-26 17:46:32 +0545

    Write js in the blade file

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -63,4 +63,20 @@
             </div>
         </div>
     </nav>
+
+    <script>
+        const dropdownElement = document.getElementById("dropdown-js");
+        const menuElement = document.getElementById("menu-js");
+        const closeElement = document.getElementById("close-js");
+
+        dropdownElement.addEventListener("click", () => {
+            menuElement.classList.toggle("hidden");
+            closeElement.classList.toggle("hidden");
+        });
+
+        closeElement.addEventListener("click", () => {
+            menuElement.classList.add("hidden");
+            closeElement.classList.add("hidden");
+        });
+    </script>
 </header>
