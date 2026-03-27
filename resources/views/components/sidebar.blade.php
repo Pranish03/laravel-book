0f3bc87e21b5ce9b1c90a19e4ea6fabf4402c015
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-03-27 11:55:23 +0545

    Check for named routes in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -7,21 +7,21 @@
         <div class="mt-8 flex flex-col gap-2 text-lg">
             <a href="{{ route('dashboard') }}"
                 class="font-medium hover:text-slate-700 hover:bg-slate-200 py-2 px-4 rounded-md flex items-center gap-3
-                {{ request()->is('admin') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
+                {{ request()->routeIs('dashboard') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
                 <i class="fa-solid fa-sliders"></i>
                 Dashboard
             </a>
 
             <a href=""
                 class="font-medium hover:text-slate-700 hover:bg-slate-200 py-2 px-4 rounded-md flex items-center gap-3
-                {{ request()->is('') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
+                {{ request()->routeIs('') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
                 <i class="fa-regular fa-address-book"></i>
                 Users
             </a>
 
             <a href="{{ route('admin.books.index') }}"
                 class="font-medium hover:text-slate-700 hover:bg-slate-200 py-2 px-4 rounded-md flex items-center gap-3
-                {{ request()->is('admin/books*') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
+                {{ request()->routeIs('admin.books.*') ? 'text-slate-700 bg-slate-100' : 'text-zinc-800' }}">
                 <i class="fa-solid fa-book"></i>
                 Books
             </a>
